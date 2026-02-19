1c350c3481dc229df4e2644f4969bf5b524a947a
eblantare <romain.blantare@gmail.com>
2026-02-19 21:18:24 +0000

    URL en dans environnementenvironment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://edt-generator-0.onrender.com/api'
+  apiUrl: 'https://edt-generator-0.onrender.com/api'  // Mettez l'URL en DUR
 };
