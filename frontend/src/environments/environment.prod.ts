9c005eac0c723c5bba79249605cfeaf1eb94239e
kaneriob <112699196+eblantare@users.noreply.github.com>
2026-02-22 03:52:36 +0000

    Configuration pour environnement de test

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://edt-generator-0.onrender.com/api'  // Mettez l'URL en DUR
+  apiUrl: 'https://edt-generator-test.onrender.com/api'
 };
